18729e1e8e322588f65a07d963ed7c8e62304d66
DoJun Lee <leedoe0709@gmail.com>
2023-12-31 01:33:40 +0900

    Title: 특정 연산을 이행하는 함수 2, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 231231/특정 연산을 이행하는 함수 2/functions-that-perform-certain-operations-2.py
+++ 231231/특정 연산을 이행하는 함수 2/functions-that-perform-certain-operations-2.py
@@ -1,28 +1,15 @@
 import math
-a, b, c = map(float, input().split())
+num = list(map(float, input().split()))
 
-if a == max([a, b, c]):
-    a = math.ceil(a)
-    if b > c:
-        b = round(b)
-        c = math.floor(c)
-    else:
-        b = math.floor(b)
-        c = round(c)
-elif b == max([a, b, c]):
-    b = math.ceil(b)
-    if a > c:
-        a = round(a)
-        c = math.floor(c)
-    else:
-        a = math.floor(a)
-        c = round(c)
-elif c == max([a, b, c]):
-    c = math.ceil(c)
-    if a > b:
-        a = round(a)
-        b = math.floor(b)
-    else:
-        a = math.floor(a)
-        b = round(b)
+a = max(num)
+num.remove(a)
+
+a = math.ceil(a)
+
+b = min(num)
+num.remove(b)
+
+b = math.floor(b)
+c = round(num[0])
+
 print(a, b, c)
