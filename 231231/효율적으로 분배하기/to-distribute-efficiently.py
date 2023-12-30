53ea2879f37fd4a976cefe259cb90136ed06fd01
DoJun Lee <leedoe0709@gmail.com>
2023-12-31 01:59:58 +0900

    Title: 효율적으로 분배하기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 231231/효율적으로 분배하기/to-distribute-efficiently.py
+++ 231231/효율적으로 분배하기/to-distribute-efficiently.py
@@ -4,7 +4,7 @@ for i in range(n // 5 + 1):
     rest = n - (i * 5)
     div = rest / 3
     if rest // 3 == div:
-        if result > i * div:
+        if result > i + div:
             result = int(i + div)
 if result == 2000:
     print(-1)
